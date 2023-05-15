20f8525488b7ccd9d9c5e3781710339eae26e67d
Comfort Ige <eligenius@gmail.com>
2023-05-15 09:42:24 +0100

    Add save btn and increment function

---
+++ index.js
@@ -0,0 +1,8 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
