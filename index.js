22da9d4f5d6f2c58915e05ead911bdf2fea5aa26
Comfort Ige <eligenius@gmail.com>
2023-05-15 09:44:56 +0100

    Add save function

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ let countEl = document.getElementById("count-el")
 let count = 0
 
 function increment() {
-    count += 1
-    countEl.textContent = count
+  count += 1
+  countEl.textContent = count
+}
+
+function save() {
+  let countStr = count + " - "
+  saveEl.textContent += countStr
+  countEl.textContent = 0
+  count = 0
 }
